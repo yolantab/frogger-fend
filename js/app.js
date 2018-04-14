639977f6278fcbc2a0a645c45cb903e1b90f33ee
Jolanta B <yolanta.b@gmail.com>
2018-04-15 00:39:01 +0200

    display game board, display player

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // Enemies our player must avoid
-var Enemy = function() {
+
+
+var Enemy = function(x, y) {
+
+
+    this.x = x;
+    this.y = y;
+    this.speed = 150;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -21,15 +30,39 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function() {
+    this.x = 200;
+    this.y = 400;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
+var allEnemies = [];
+
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
+player.handleInput = function() {
+
+}
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -173,7 +173,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+		'images/char-cat-girl.png',
+		'images/char-horn-girl.png',
+		'images/char-pink-girl.png',
     ]);
     Resources.onReady(init);
 
