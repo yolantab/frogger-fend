1f11ca8a1f96b7a8b75e7ba486045f83e410dd29
Jolanta B <yolanta.b@gmail.com>
2018-04-16 19:26:09 +0200

    collision check added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var Enemy = function(x, y) {
     this.x = x;
     this.y = y;
     //rtandom spee between 80 and 120 (120-80=40)
-    this.speed = Math.floor(Math.random() * 41) + 80;
+    this.speed = Math.floor(Math.random() * 41) + 70;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -16,7 +16,7 @@ var Enemy = function(x, y) {
 
 };
 var enemy1 = new Enemy(1, 50);
-var enemy2 = new Enemy(-200, 50);
+var enemy2 = new Enemy(-250, 50);
 var enemy3 = new Enemy(20, 150);
 var enemy4 = new Enemy(10, 230);
 var enemy5 = new Enemy(-200, 230);
@@ -45,24 +45,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
 
 var Player = function() {
     this.x = 200;
     this.y = 400;
-    this.sprite = 'images/char-cat-girl.png';
+    this.sprite = 'images/char-horn-girl.png';
+
 };
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 var player = new Player();
-var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
+
 
 
 Player.prototype.update = function(dt) {
     // don't allow player to  move out of the board
+
     if (this.x >= 430 || this.x <= -30) {
         if (this.x >= 430) {
             this.x -= 30;
@@ -71,10 +73,12 @@ Player.prototype.update = function(dt) {
         }
 
     }
+    this.checkCollisions();
+
     if (this.y <= -30 || this.y >= 430) {
         if (this.y <= -30) {
             counter += 1;
-            count.innerHTML = `counter: ${counter}`;
+            count.innerHTML = `score: ${counter}`;
             this.reset();
         } else {
             this.reset();
@@ -88,12 +92,28 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// collisions with enemy
+
+Player.prototype.checkCollisions = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+
+        if (this.x < allEnemies[i].x + 65 &&
+            this.x + 65 > allEnemies[i].x &&
+            this.y < allEnemies[i].y + 65 &&
+            65 + this.y > allEnemies[i].y) {
+            this.reset();
+            counter = 0;
+            count.innerHTML = `score: ${counter}`;
+        }
+    }
+};
+
 // reset player position
 Player.prototype.reset = function() {
-        this.x = 200;
-        this.y = 400;
-    }
-    //move player
+    this.x = 200;
+    this.y = 400;
+};
+//move player
 player.handleInput = function(key) {
 
     if (key === 'left') {
@@ -106,7 +126,14 @@ player.handleInput = function(key) {
         this.y += 30;
     }
 
-}
+};
+
+
+
+
+
+
+
 
 
 // This listens for key presses and sends the keys to your
@@ -121,4 +148,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-7
